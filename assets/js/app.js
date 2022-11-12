8953941152eefb7893a974c3f3685aeab3529e29
Faysal-Wahid <mdfaysalwahid@gmail.com>
2022-11-12 23:13:22 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+//! === ADD HEADER STYLES ON SCROLL ===
+const headerScroll = () => {
+    const headerElement = document.querySelector('.header');
+    this.scrollY >= 30 ? headerElement.addEventListener('active') : headerElement.classList.remove('active');
+}
+
+window.addEventListener('scroll', headerScroll);
+//! === OPEN AND CLOSE THE MENU ON HAMBURGER ICON CLICK ===
+const menuToggler = document.querySelector('#menu-toggler');
+const navbarMenu = document.querySelector('.navbar_menu');
+const toggleMenu = () => {
+    navbarMenu.classList.toggle('active');
+
+}
+menuToggler.addEventListener('click', toggleMenu);
+// --- CLOSE MENU WHEN NAV-LINKS ARE CLICKED ---
+const linksToggleMenu = (e) => {
+    if(e.target.classList.contains('navbar_list-link')) navbarMenu.classList.remove('active');
+}
+window.addEventListener('click', linksToggleMenu);
+//! === SWIPER ===
+
+//! === SCROLL REVEAL ===
